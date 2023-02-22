39bd0b1473992ca744cb4ad8a0823bcbb3ecd0f4
Amako Aoi <amako.aoi@gmail.com>
2023-02-22 15:00:21 -0500

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,12 @@ export default defineConfig({
                 compilerOptions: {
                     isCustomElement: (tag) => tag.startsWith("sl-"),
                 },
+            },
+        }),
+        VitePWA({
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
             },
         }),
     ],
